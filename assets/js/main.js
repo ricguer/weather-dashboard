8913cdd984f592c86424282da7a7c697eac431f3
Ricardo Guerra <rj.guerra@outlook.com>
2023-05-24 14:41:31 -0400

    Create Procedure to Average Full 5 Day Forecast
    - The default response of the API provides a 5 day forecast at 3 hour increments. Add a method that condenses this data into a single array containing a single set of details per day.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,70 @@
-let apiURL = "http://api.openweathermap.org/";
-let openWeatherKeyAPI = "2dac5e788766a237de5453bf4c85309a";
+                                                                /* ================= GLOBAL VARIABLES ================= */
+const apiURL = "http://api.openweathermap.org/";                /* API URL                                              */
+const openWeatherKeyAPI = "2dac5e788766a237de5453bf4c85309a";   /* API Key                                              */
+const rowOfCards = $("#card-row");
+
+                                                                /* Object: stores weather data per card (day)           */
+function DayWeather(date, 
+                    icon, 
+                    iconAltText, 
+                    temperature, 
+                    wind, 
+                    humidity) 
+{
+    this.date         =  date;
+    this.icon         =  icon;
+    this.iconAltText  =  iconAltText;
+    this.temperature  =  temperature;
+    this.wind         =  wind;
+    this.humidity     =  humidity;
+}
 
 $(function () 
 {
-    // createFiveDayForecast();
+    let fiveDayForecast = fetchFiveDayForecast("Miami");        /* Fetch full five day forecast of default city         */
+    createFiveDayForecast(fiveDayForecast);                     /* Create five day forecast weather dashboard           */
 });
 
 function fetchFiveDayForecast(cityName) 
 {
     let processedCityName = cityName.replace(/\s+/g, "%20");
-    let requestURL = apiURL + "data/2.5/forecast?q=" + processedCityName + "&appid=" + openWeatherKeyAPI;
+    let requestURL = apiURL + "data/2.5/forecast?q=" + processedCityName + "&units=imperial" + "&appid=" + openWeatherKeyAPI;
     let fiveDayForecast =  fetch(requestURL)
                             .then((response) => response.json())
+
+    console.log(fiveDayForecast);
 
     return fiveDayForecast;
 }
 
 function averageFiveDayForecast(fullFiveDayForecast) 
 {
-    let averageFiveDayObj;
+    let averageFiveDay = [];
 
+    for (let forecastIndex = 0; forecastIndex < fullFiveDayForecast.list.length; forecastIndex += 8) {
+        const element = fullFiveDayForecast.list[forecastIndex];
+        
+        averageFiveDay.push(new DayWeather( dayjs(element.dt_txt).format("MM/DD/YYYY"), 
+                                            "./assets/images/sun.svg", 
+                                            "Icon Alt Text", 
+                                            element.main.temp, 
+                                            element.wind.speed, 
+                                            element.main.humidity));
+    }
 
+    return averageFiveDay;
 }
+
+function setWeatherIcon(weatherDescription) {
+    
+}
+
+
 
 function createFiveDayForecast(fiveDayForecast) 
 {
+    rowOfCards.empty();
+
     fiveDayForecast.then((data) => {
 
         let forecastFiveDays = averageFiveDayForecast(data);
@@ -38,14 +78,14 @@ function createFiveDayForecast(fiveDayForecast)
             let cardTitle = $("<h5>", 
             {
                 class: "card-title",
-                text: forecastFIveDays[dayIndex].date
+                text: forecastFiveDays[dayIndex].date
             });
         
             let cardImage = $("<img>", 
             {
                 class: "card-img-top",
-                src: forecastFIveDays[dayIndex].icon,
-                alt: forecastFIveDays[dayIndex].iconAltText
+                src: forecastFiveDays[dayIndex].icon,
+                alt: forecastFiveDays[dayIndex].iconAltText
             });
         
             let cardListGroup = $("<ul>", 
@@ -56,26 +96,26 @@ function createFiveDayForecast(fiveDayForecast)
             let tempItem = $("<li>",
             {
                 class: "list-group-item",
-                text: forecastFIveDays[dayIndex].temperature
+                text: "Temperature: " + forecastFiveDays[dayIndex].temperature + " °F"
             });
         
             let windItem = $("<li>",
             {
                 class: "list-group-item",
-                text: forecastFIveDays[dayIndex].wind
+                text: "Wind: " + forecastFiveDays[dayIndex].wind + " mph"
             });
         
             let humidityItem = $("<li>",
             {
                 class: "list-group-item",
-                text: forecastFIveDays[dayIndex].humidity
+                text: "Humidity: " + forecastFiveDays[dayIndex].humidity + "%"
             });
         
             cardListGroup.append(tempItem, windItem, humidityItem);
             cardBody.append(cardTitle, cardImage);
             card.append(cardBody, cardListGroup);
             cardColDiv.append(card);
-            cardColDiv.appendTo("#card-row");
+            cardColDiv.appendTo(rowOfCards);
         }
     });
 }
